d62c1d57314b39c767b23e03cbbee36852742589
Andrew Knapp <acknapp@gmail.com>
2022-01-30 14:49:00 -0800

    added ninja animation for the character, and notes in the artifact

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,6 +20,9 @@ onready var greyDragon = $Sprites/GreyDragon
 onready var ninja = $Sprites/Ninja
 onready var stats = $Health
 onready var hurtInvincibilityTimer = $HurtInvincibilityTimer
+onready var animationTree = $AnimationTree
+onready var animationPlayer = $AnimationPlayer
+onready var animationState = animationTree.get("parameters/playback")
 
 # TODO:
 var velocity = Vector2.ZERO
@@ -27,13 +30,15 @@ var state = MOVE
 var transforming = false
 var disguise = null
 var invincible := false
+var artifact_obtained = false
 
 signal die()
 
 func _ready():
 	disguise = ninja
 	hurtInvincibilityTimer.connect("timeout", self, "_on_Hurtbox_invincibility_timeout")
-
+	# TODO: signal connection not yet working. Find a way to indicate that the player has stolen an artifact
+	connect("stolen", self, "stolen_artifact") 
 
 func _physics_process(delta):
 	match state:
@@ -41,21 +46,22 @@ func _physics_process(delta):
 			move_state(delta)
 
 
-
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-
+	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/NinjaIdle/blend_position", input_vector)
+		animationTree.set("parameters/NinjaWalk/blend_position", input_vector)
+		animationState.travel("NinjaWalk")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("NinjaIdle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
+	
 	velocity = move_and_slide(velocity)
-
-	# TODO: Add steal and transform code here
 
 
 func choose_disguise():
@@ -76,30 +82,39 @@ func hide_disguises():
 	for disguise in all_disguises:
 		disguise.visible = false
 
-func hurt():
-	printerr("HURT")
-	# play hurt animation
-	# bounce away?
-	stats.health = stats.health - 1
-	if stats.health <= 0:
-		die()
-		return
-
-	invincible = true
 
 func die():
 	# death animation?
 	emit_signal("die")
 
+
 func _on_ChangingHitBox_area_entered(area):
 	hide_disguises()
 	choose_disguise()
-	# yield(choose_disguise(), "disguise_decision_made")
+	# yield(choose_disguise(), "disguise_decision_made") # Doesn't work
 	$ChangingHitBox/ChangingTimer.start()
 
 
 func _on_ChangingTimer_timeout():
 	disguise.visible = true
 
+
 func _on_Hurtbox_invincibility_timeout():
 	invincible = false
+
+
+func hurt():
+	printerr("HURT")  # DEBUG
+	# play hurt animation
+	# bounce away?
+	stats.health = stats.health - 1
+	if stats.health <= 0:
+		die()
+		return
+		
+	invincible = true
+
+
+func steal_artifact():
+	print("artifact stolen") # DEBUG
+	artifact_obtained = true
--- World/Artifact.gd
+++ World/Artifact.gd
@@ -3,5 +3,6 @@ extends Area2D
 signal stolen
 
 func _on_Artifact_body_entered(body):
+	$StealArtifact.play()
 	$Sprite.visible = false
 	emit_signal("stolen")
